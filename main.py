cc23898dff21a58fdfb8831589a56a6511961e00
Vadim Goncharov <vadim.goncharov.1999@gmail.com>
2022-11-17 20:22:38 +0200

    роблено сортування таблиці на вибір користувача

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from expense import Expense
-from prettytable import PrettyTable
 from prettytable import from_csv
+from prettytable import PrettyTable
 import os
 import csv
 import argparse
@@ -31,13 +31,34 @@ def replase_row_in_file(row_count:int, file_p:str, new_data:str):
     with open(file_p, 'w') as file:
         file.writelines(old_file)
 
-def read_file():
-    global file_path
-    global file_name
+def print_table_with_sort(key:str, revers = False):
+    path = file_path + file_name
+    if(os.path.exists(path)):
+        table = PrettyTable(field_names=['Дата', 'Ціна', 'Назва', 'Тип'])
+        with open(path, 'r') as file:
+            reader = csv.reader(file, delimiter=';')
+            for line in reader:
+                line[1] = int(line[1])
+                table.add_row(line)
+            table.align["Ціна"] = 'r'
+            table.align["Назва"] = 'l'
+        try: 
+            table.sortby = key    
+        except:
+            print('Такого стовпця не існує, сортування відміняється')
+        table.reversesort = revers
+        print(table)
+    else: print('Файла поки що не існує. Спочатку стфоріть файл') 
+
+def print_table_without_sort():
     path = file_path+file_name
     if(os.path.exists(path)):
-        with open(path, 'r') as file_db:
-            table = from_csv(file_db)
+        with open(path, 'r') as file:
+            table = from_csv(file, field_names=['Дата', 'Ціна', 'Назва', 'Тип'])
+        table.align["Ціна"] = 'r'
+        table.align["Назва"] = 'l'
+        table.sortby = 'Ціна'
+
         print(table)
     else:
         print('Файла поки що не існує. Спочатку стфоріть файл') 
@@ -60,19 +81,19 @@ def main():
                                     argument_default=False
                                     )
     parser.add_argument('--path', '-p', 
-                        help='Обрати розташування файла',
+                        help='Обрати розташування файла з таблицею. Приклад: -p "C:\\Users\Public\\Documents"',
                         default=None,
                         type=str,
                         required=False
                         )
     parser.add_argument('--name', '-n',
-                        help='Обрати назву файла',
+                        help='Обрати назву файла з таблицею. Приклад: -n new_file',
                         default=None,
                         type=str,
                         required=False
                         )
     parser.add_argument('--write', '-w',
-                        help='Записати у файл нову строку. Якщо не вказано дату, то записує сьогоднішню. Якщо не вказано тип, то записує "Різне" Приклад: -w <name> <cost> (optional): -d <date> -t <type>',
+                        help='Записати у файл нову строку. Якщо не вказано дату, то записує сьогоднішню. Якщо не вказано тип, то записує "Різне" Приклад: -w "Молоко та сир" 268   (за бажанням): -d 25.09.2022 -t Їжа',
                         nargs=2,
                         metavar=('name', 'cost'),
                         required=False
@@ -90,7 +111,29 @@ def main():
                         action='store_true',
                         required=False
                         )
-    
+    parser.add_argument('--sortcost', '-sc',
+                        help="Доповнення до аргументу READ. Сортування за ціною. Приклад: -r -sc",
+                        action='store_true',
+                        required=False
+                        )
+    parser.add_argument('--sortname', '-sn',
+                        help="Доповнення до аргументу READ. Сортування за назвою. Приклад: -r -sn",
+                        action='store_true',
+                        required=False
+                        )
+    parser.add_argument('--sortdate', '-sd',
+                        help="Доповнення до аргументу READ. Сортування за датою. Приклад: -r -sd",
+                        action='store_true',
+                        required=False
+                        )
+    parser.add_argument('--revers', '-re',
+                        help="Доповнення до аргументу READ. Вмикає зворотнє сортування. Приклад: -r -sc -r",
+                        action='store_true',
+                        required=False
+                        )
+
+
+
     args = parser.parse_args()
         
     if args.path != None:
@@ -100,7 +143,10 @@ def main():
         set_file_name(args.name)
         
     if args.read == True:
-        read_file()
+        if args.sortcost != False: print_table_with_sort('Ціна', args.revers)
+        elif args.sortname != False: print_table_with_sort('Назва', args.revers)
+        elif args.sortdate != False: print_table_with_sort('Дата', args.revers)
+        else: print_table_without_sort()
         
     if args.write != False:
         name, cost = args.write
